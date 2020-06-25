b14de95da3cb346c5efe3a7c2d93df90ccb3dd43
Andre Wolden <wolden.andre@gmail.com>
2020-06-25 18:13:06 +0200

    retter Fetcher example

--- src/app/functional/fetcher/example-usage/FpEssentialsExample.tsx
+++ src/app/functional/fetcher/example-usage/FpEssentialsExample.tsx
@@ -1,20 +1,20 @@
 import * as React from 'react';
-import { SøkerApiResponse, SøkerP, søkerRecipe } from '../../../types/apiTypes/søkerTypes';
+import { SøkerApiResponse, søkerRecipe } from '../../../types/apiTypes/søkerTypes';
 import FpFetcherSuccessView from './FpFetcherSuccessView';
 import FpError from './FpError';
 import LoadingPage from '../../../components/pages/loading-page/LoadingPage';
-import { BarnApiResponse, BarnP, barnRecipe } from '../../../types/apiTypes/barnTypes';
+import { BarnApiResponse, barnRecipe } from '../../../types/apiTypes/barnTypes';
 import Fetcher2 from '../Fetcher2';
 import Fetcher from '../Fetcher';
 import Fetcher3 from '../Fetcher3';
-import { ArbeidsgiverApiResponse, ArbeidsgiverP, arbeidsgiverRecipe } from '../../../types/apiTypes/arbeidsgiverTypes';
+import { ArbeidsgiverApiResponse, arbeidsgiverRecipe } from '../../../types/apiTypes/arbeidsgiverTypes';
 
 const FpEssentialsExample: React.FC = () => {
     return (
         <>
             <div>-----------------------------</div>
             {/* Fetch 1 ressurs */}
-            <Fetcher<SøkerP, SøkerApiResponse>
+            <Fetcher<SøkerApiResponse>
                 recipies={[søkerRecipe]}
                 loading={() => <LoadingPage />}
                 error={(e: Error) => <FpError error={e} />}
@@ -24,7 +24,7 @@ const FpEssentialsExample: React.FC = () => {
             />
             <div>-----------------------------</div>
             {/* Fetch 2 ressurser */}
-            <Fetcher2<SøkerP, SøkerApiResponse, BarnP, BarnApiResponse>
+            <Fetcher2<SøkerApiResponse, BarnApiResponse>
                 recipies={[søkerRecipe, barnRecipe]}
                 loading={() => <LoadingPage />}
                 error={(e: Error) => <FpError error={e} />}
@@ -34,7 +34,7 @@ const FpEssentialsExample: React.FC = () => {
             />
             <div>-----------------------------</div>
             {/* Fetch 3 ressurser */}
-            <Fetcher3<SøkerP, SøkerApiResponse, BarnP, BarnApiResponse, ArbeidsgiverP, ArbeidsgiverApiResponse>
+            <Fetcher3<SøkerApiResponse, BarnApiResponse, ArbeidsgiverApiResponse>
                 recipies={[søkerRecipe, barnRecipe, arbeidsgiverRecipe]}
                 loading={() => <LoadingPage />}
                 error={(e: Error) => <FpError error={e} />}
--- src/app/functional/utilities.ts
+++
@@ -1,9 +0,0 @@
-import * as IoTs from 'io-ts';
-
-export const stringEnum = <T>(enumObj: T, enumName = 'enum'): IoTs.Type<T[keyof T], string> =>
-    new IoTs.Type<T[keyof T], string>(
-        enumName,
-        (u): u is T[keyof T] => Object.values(enumObj).includes(u),
-        (u, c) => (Object.values(enumObj).includes(u) ? IoTs.success(u as T[keyof T]) : IoTs.failure(u, c)),
-        (a) => (a as unknown) as string
-    );
