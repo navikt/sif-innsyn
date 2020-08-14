92b5cd111e7bb50a4a7eace51d051daad40b20d9
Sander Opperud Odden <sander.opperud.odden@nav.no>
2020-08-14 11:19:59 +0200

    Utvider med omsorgspenger

--- src/app/utils/SøknadUtils.ts
+++ src/app/utils/SøknadUtils.ts
@@ -3,6 +3,14 @@ import { Søknad, Søknadstype } from '../types/apiTypes/søknadTypes';
 export const erPleiepenger = (søknad: Søknad) => {
     return søknad.søknadstype == Søknadstype.PP_ETTERSENDING || søknad.søknadstype == Søknadstype.PP_SYKT_BARN;
 };
+export const erOmsorgspenger = (søknad: Søknad) => {
+    return (
+        søknad.søknadstype == Søknadstype.OMP_UTVIDET_RETT ||
+        søknad.søknadstype == Søknadstype.OMP_UTBETALING_SNF ||
+        søknad.søknadstype == Søknadstype.OMP_UTBETALING_ARBEIDSTAKER ||
+        søknad.søknadstype == Søknadstype.OMP_ETTERSENDING
+    );
+};
 
 export const søknadTypeErPleiepengerNærstående = (type: Søknadstype) => {
     return type === Søknadstype.PP_NÆRSTÅENDE;
