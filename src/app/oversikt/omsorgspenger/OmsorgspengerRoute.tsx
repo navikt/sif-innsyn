937268d6cab0e485161ae186bee963953e940c6b
Sander Opperud Odden <sander.opperud.odden@nav.no>
2020-08-20 13:38:14 +0200

    Fix fetcher

--- src/app/oversikt/omsorgspenger/OmsorgspengerRoute.tsx
+++ src/app/oversikt/omsorgspenger/OmsorgspengerRoute.tsx
@@ -1,15 +1,22 @@
 import * as React from 'react';
 import LoadingPage from '../../components/pages/loading-page/LoadingPage';
-import FpError from '../../functional/fetcher/example-usage/FpError';
 import Fetcher from '../../functional/fetcher/Fetcher';
 import { SøknadApiResponse, søknadRecipe } from '../../types/apiTypes/søknadTypes';
 import OmsorgspengerView from './OmsorgspengerView';
+import HandleUnauthorized from '../../functional/HandleUnauthorized';
+import ReactJsonView from '../../functional/ReactJsonView';
 
 const OmsorgspengerRoute: React.FC = (): JSX.Element => (
     <Fetcher<SøknadApiResponse>
         recipies={[søknadRecipe]}
         loading={() => <LoadingPage />}
-        error={(e: Error) => <FpError error={e} />}
+        error={(e: Error) => (
+            <HandleUnauthorized
+                error={e}
+                onWillRedirect={() => <LoadingPage />}
+                handleError={() => <ReactJsonView input={e} />} // TODO: Håndter error.
+            />
+        )}
         success={([søknadApiResponse]: [SøknadApiResponse]) => <OmsorgspengerView søknader={søknadApiResponse} />}
     />
 );
