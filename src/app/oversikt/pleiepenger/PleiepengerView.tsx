8f8947b78856ead6ed5934735bd7e658e783e410
Andreas Nordahl <andnorda@gmail.com>
2020-09-14 13:56:44 +0200

    add content to pleiepenger view

--- src/app/oversikt/pleiepenger/PleiepengerView.tsx
+++ src/app/oversikt/pleiepenger/PleiepengerView.tsx
@@ -3,11 +3,14 @@ import { SøknadApiResponse } from '../../types/apiTypes/søknadTypes';
 import Lesmerpanel from 'nav-frontend-lesmerpanel';
 import './PleiepengerView.less';
 import SoknadstatusinfoComponent from 'app/components/soknadstatusinfoComponent/SoknadstatusinfoComponent';
-import { Ingress, Sidetittel, Undertittel } from 'nav-frontend-typografi';
+import { Ingress, Normaltekst, Sidetittel, Undertittel } from 'nav-frontend-typografi';
 import { Tilbakeknapp } from 'nav-frontend-ikonknapper';
 import { useHistory } from 'react-router-dom';
 import { RouteConfig } from '../../config/routeConfig';
 import Ekspanderbartpanel from 'nav-frontend-ekspanderbartpanel';
+import ProcessDescription from 'app/components/process-description/ProcessDescription';
+import ExpandableInfo from 'app/components/expandable-info/ExpandableInfo';
+import Lenke from 'nav-frontend-lenker';
 
 interface Props {
     søknader: SøknadApiResponse;
@@ -45,7 +48,87 @@ const PleiepengerView: React.FC<Props> = ({ søknader }: Props) => {
 
                     <Undertittel>Nyttig informasjon</Undertittel>
                     <Ekspanderbartpanel tittel="Dette kan du forvente deg etter at du har sendt søknad">
-                        todo
+                        <ProcessDescription
+                            steps={[
+                                <>
+                                    <Undertittel>Du har sendt søknad med legeerklæring</Undertittel>
+                                    <Normaltekst>
+                                        Hvis du ikke har sendt legeerklæring med søknaden din, må du{' '}
+                                        <Lenke href="#TODO">ettersende denne</Lenke> så snart du kan
+                                    </Normaltekst>
+                                </>,
+                                <>
+                                    <Undertittel>Arbeidsgiveren din sender inntektsmelding til oss</Undertittel>
+                                    <ExpandableInfo title="Hva betyr dette?">
+                                        <Normaltekst>
+                                            Du må selv si fra til arbeidsgiveren din om at de må sende en
+                                            inntektsmelding til oss. Inntektsmeldingen gir oss blant annet informasjon
+                                            om lønna di, og om arbeidsgiver vil ha refusjon eller ei.
+                                        </Normaltekst>
+                                    </ExpandableInfo>
+                                </>,
+                                <div style={{ display: 'grid', gridGap: '0.5em' }}>
+                                    <div>
+                                        <Undertittel>Vi ser på søknaden din og behandler den</Undertittel>
+                                        <Lenke href="#TODO">Se saksbehandlingstider der du bor</Lenke>
+                                    </div>
+
+                                    <Undertittel>Vi kontakter deg hvis vi trenger mer informasjon</Undertittel>
+
+                                    <div>
+                                        <Undertittel>
+                                            Hvis noe i situasjonen din endrer seg må du si fra til oss
+                                        </Undertittel>
+                                        <ExpandableInfo title="Hvilke endringer må jeg si fra om?">
+                                            <Normaltekst>For å unngå feil må du straks gi beskjed hvis:</Normaltekst>
+                                            <ul>
+                                                <li> barnet ikke lenger har behov for kontinuerlig omsorg og pleie.</li>
+                                                <li>barnet begynner eller øker tiden i et omsorgstilbud.</li>
+                                                <li>du begynner å jobbe igjen, eller øker antall arbeidstimer.</li>
+                                                <li>omsorgen for barnet er overført til andre, helt eller delvis.</li>
+                                                <li>du får omsorgsstønad fra kommunen.</li>
+                                                <li>du skal ha lovbestemt ferie. Hva er lovbestemt ferie?</li>
+                                                <li>du skal til utlandet.</li>
+                                            </ul>
+                                            <Normaltekst>
+                                                Du melder fra om endringer ved å skrive en beskjed til oss. Du kan også
+                                                ringe 55 55 33 33, tastevalg 3.
+                                            </Normaltekst>
+                                        </ExpandableInfo>
+                                    </div>
+                                </div>,
+                                <>
+                                    <Undertittel>
+                                        Når søknaden er ferdig behandlet får du tilsendt svar i saksoversikten på Ditt
+                                        NAV og i posten.
+                                    </Undertittel>
+                                    <ExpandableInfo title="Hvilke endringer må jeg si fra om?">
+                                        <Normaltekst>
+                                            Når vi har sett på saken din vil vi sende deg et svar på søknaden din. I
+                                            svaret vil det stå om du har fått innvilget det du har søkt om.
+                                        </Normaltekst>
+                                    </ExpandableInfo>
+                                </>,
+                                <>
+                                    <Undertittel>
+                                        Hvis du får innvilget pleiepenger vil det registreres i saksoversikten din at du
+                                        skal ha utbetaling
+                                    </Undertittel>
+                                    <ExpandableInfo title="Når får jeg utbetaling?">
+                                        <Normaltekst>
+                                            Utbetaling av pleiepenger fra NAV er i slutten av hver måned. Hvis du har
+                                            fått innvilget pleiepenger for en periode tilbake i tid sender vi utbetaling
+                                            for passerte måneder med en gang.
+                                        </Normaltekst>
+                                        <Normaltekst>
+                                            Hvis arbeidsgiveren din skal ha refusjon (utbetale lønn til deg som vanlig
+                                            og få tilbake pengene fra NAV) må du høre med arbeidsgiver om det er noe du
+                                            lurer på angående lønnen din.
+                                        </Normaltekst>
+                                    </ExpandableInfo>
+                                </>,
+                            ]}
+                        />
                     </Ekspanderbartpanel>
                 </div>
             )}
