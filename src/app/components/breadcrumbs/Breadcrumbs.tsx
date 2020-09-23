0e8f8a995a30788f666deb832c2dd1869b2c0446
Frode Hansen <frode.hansen@bekk.no>
2020-09-23 19:18:55 +0200

    Refactor breadcrumbs

--- src/app/components/breadcrumbs/Breadcrumbs.tsx
+++ src/app/components/breadcrumbs/Breadcrumbs.tsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import NavFrontendChevron from 'nav-frontend-chevron';
+// import NavFrontendChevron from 'nav-frontend-chevron';
 import Lenke from 'nav-frontend-lenker';
 import { RouteConfig } from '../../config/routeConfig';
 import getLenker from '../../lenker';
 import bemUtils from '../../utils/bemUtils';
-// import { getEnvironmentVariable } from '../../utils/envUtils';
 import useWindowSize from '../../utils/useWindowSize';
-import DittNavnIconSvg from '../ditt-nav-breadcrumbs/DittNavnIconSvg';
+import DittNavIconSvg from '../ditt-nav-breadcrumbs/DittNavnIconSvg';
 import './breadcrumbs.less';
 
-const cls = bemUtils('breadcrumbs');
+const bem = bemUtils('breadcrumbs');
 
 export interface Breadcrumb {
-    route: string;
+    href?: string;
+    route?: string;
     title: string;
 }
 
@@ -24,66 +25,54 @@ interface OwnProps {
 
 type Props = OwnProps;
 
+const Crumb = ({ title, href, route, isCurrent }: Breadcrumb & { isCurrent?: boolean }) => (
+    <div className={bem.element('crumb', isCurrent ? 'current' : undefined)}>
+        {route && <Link to={route}>{title}</Link>}
+        {href && <Lenke href={href}>{title}</Lenke>}
+        {!route && !href && <div>{title}</div>}
+    </div>
+);
+
 const Breadcrumbs = (props: Props) => {
     const { width } = useWindowSize();
     const { currentPageTitle, crumbs = [] } = props;
-    const frontpageUrl = '/'; //getEnvironmentVariable('PUBLIC_PATH');
+    const frontpageUrl = '/';
 
-    const crumbsToRender: React.ReactNode[] = [];
-    const parentPageUrl = crumbs.length > 0 ? crumbs[crumbs.length - 1].route : frontpageUrl;
+    let crumbsToRender: Array<Breadcrumb> = [];
 
     if (width && width < 576) {
-        crumbsToRender.push(
-            <div key="chevron" aria-hidden={true}>
-                <NavFrontendChevron type="venstre" />
-            </div>
-        );
-        crumbsToRender.push(
-            <div key="forrige-side" className={cls.element('item')}>
-                <Link to={parentPageUrl}>Tilbake</Link>
-            </div>
-        );
+        crumbsToRender = [
+            {
+                title: 'Tilbake',
+                href: crumbs.length > 0 ? crumbs[crumbs.length - 1].route : undefined,
+                route: crumbs.length === 0 ? frontpageUrl : undefined,
+            },
+        ];
     } else {
-        crumbsToRender.push(<DittNavnIconSvg key={'dittNavIkon'} />);
-        crumbsToRender.push(
-            <div key="dittNAV" className={cls.element('item')}>
-                <Lenke href={getLenker().dittNAV} title="Gå til ditt nav forsiden">
-                    Ditt NAV
-                </Lenke>
-            </div>
-        );
-        crumbsToRender.push(
-            <div key={`chevron_page`} aria-hidden={true}>
-                <NavFrontendChevron type="høyre" />
-            </div>
-        );
-        crumbsToRender.push(
-            <div key="oversikt" className={cls.element('item')}>
-                <Link to={RouteConfig.OVERSIKT}>Sykdom i familien</Link>
-            </div>
-        );
-        crumbs.forEach((crumb) => {
-            crumbsToRender.push(
-                <div key={`chevron_page_${crumb.route}`} aria-hidden={true}>
-                    <NavFrontendChevron type="høyre" />
-                </div>
-            );
-            crumbsToRender.push(
-                <Link key={crumb.route} to={crumb.route}>
-                    {crumb.title}
-                </Link>
-            );
-        });
-        crumbsToRender.push(
-            <div key={`chevron_page_2`} aria-hidden={true}>
-                <NavFrontendChevron type="høyre" />
-            </div>
-        );
-        crumbsToRender.push(<div key={`currentPage`}>{currentPageTitle}</div>);
+        crumbsToRender = [
+            {
+                title: 'Ditt NAV',
+                href: getLenker().dittNAV,
+            },
+            {
+                title: 'Sykdom i familien',
+                route: RouteConfig.OVERSIKT,
+            },
+            ...crumbs,
+            { title: currentPageTitle },
+        ];
     }
     return (
-        <nav aria-label="Du er her" className={cls.block}>
-            {crumbsToRender}
+        <nav aria-label="Du er her" className={bem.block}>
+            {crumbsToRender.length > 1 && <DittNavIconSvg key={'dittNavIkon'} />}
+            {crumbsToRender.map((c, idx) => (
+                <>
+                    <span role="presentation" aria-hidden={true}>
+                        <NavFrontendChevron type="høyre" />
+                    </span>
+                    <Crumb key={idx} {...c} isCurrent={idx === crumbsToRender.length - 1} />
+                </>
+            ))}
         </nav>
     );
 };
--- src/app/components/ditt-nav-breadcrumbs/DittNavBreadcrumbs.tsx
+++ src/app/components/ditt-nav-breadcrumbs/DittNavBreadcrumbs.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Lenke from 'nav-frontend-lenker';
 import bemUtils from '../../utils/bemUtils';
-import DittNavnIconSvg from './DittNavnIconSvg';
+import DittNavIconSvg from './DittNavnIconSvg';
 import './dittNavBreadcrumbs.less';
 import getLenker from '../../lenker';
 
@@ -10,7 +10,7 @@ const bem = bemUtils('dittNavBreadcrumbs');
 const DittNavBreadcrumbs = () => (
     <div className={bem.block}>
         <div className={bem.element('icon')} role="presentation" aria-hidden={true}>
-            <DittNavnIconSvg />
+            <DittNavIconSvg />
         </div>
         <Lenke href={getLenker().dittNAV}>Ditt NAV</Lenke> <span className={bem.element('separator')}>/</span> Sykdom i
         familien
