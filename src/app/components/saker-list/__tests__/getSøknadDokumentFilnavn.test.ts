eb490f4148f2c642aae53508f60802965a5f08f7
Frode Hansen <frode.hansen@bekk.no>
2022-03-09 06:02:26 +0100

    Oppdatere tester

--- src/app/components/saker-list/__tests__/getSøknadDokumentFilnavn.test.ts
+++ src/app/components/saker-list/__tests__/getSøknadDokumentFilnavn.test.ts
@@ -7,15 +7,12 @@ jest.mock('../../../utils/envUtils.ts', () => {
 
 describe('getSøknadDokumentFilnavn', () => {
     it('returnerer riktig format på dokument filnavn', () => {
-        const result = getSøknadDokumentFilnavn(
-            {
-                tittel: 'Søknad om pleiepenger',
-                dokumentInfoId: 'abc',
-                filtype: 'PDF',
-                url: 'none',
-            },
-            new Date(2021, 0, 1)
-        );
+        const result = getSøknadDokumentFilnavn({
+            tittel: 'Søknad om pleiepenger',
+            dokumentInfoId: 'abc',
+            filtype: 'PDF',
+            url: 'none',
+        });
         expect(result).toEqual('s%C3%B8knad%20om%20pleiepenger%20(01%2E01%2E2021).pdf');
     });
 });
