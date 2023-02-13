c1510e8df53a306b40606529c2d2e66b783c6589
Aleksei Poroshin <aleksei.poroshin@nav.no>
2023-02-13 11:11:27 +0100

    pkg upd

--- src/app/utils/appSentryLogger.ts
+++ src/app/utils/appSentryLogger.ts
@@ -1,6 +1,6 @@
 import getSentryLoggerForApp from '@navikt/sif-common-sentry';
 import { APPLICATION_KEY } from '../App';
 
-const appSentryLogger = getSentryLoggerForApp(APPLICATION_KEY);
+const appSentryLogger = getSentryLoggerForApp(APPLICATION_KEY, ['sif-innsyn.dev.nav.no/dist/js'], [/401/]);
 
 export default appSentryLogger;
